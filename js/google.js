e90cd484f09e545898876a958b360db3bf816ec2
Josh Horwitz <jhorwit2@gmu.edu>
2014-09-30 09:28:54 -0400

    added click tracking. fixed typo

--- js/google.js
+++ js/google.js
@@ -5,3 +5,9 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-55214329-1', 'auto');
 ga('send', 'pageview');
+
+
+
+$('a').click(function(event) {
+    ga('send', 'event', 'button', 'click', event.currentTarget.href);
+});
